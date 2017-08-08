3d491772b24b9980d46390d3bf953aeaebee79ea
Timothy B <charliebrown1a@gmail.com>
2017-08-07 22:29:42 -0700

    Finished Lab 3 Part 1
    I'm submitting this new Lab 3 part 1 file, attached is a file showing my "ls -l", I believe it is outputting right, but not sure, also the use of 'if' statements is not my original idea for this project, I was guided to use that, I tried switch statements, but unable to use them.

--- Lab3.c
+++ Lab3.c
@@ -1,54 +1,86 @@
 
 
-
-
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 
-int main(int argc, char *argv[])
-{
-   	int i,j;
-	printf("--Start of Lab 2 program--\n");
-	int cUID = getuid();
-	int cGID = getgid();
-	printf("UID: %d\n", cUID);
-	printf("GID: %d\n", cGID);
-  	pid_t pid = fork();
+  int main(int argc, char * argv[]) {
+    int i, j;
+    printf("--Start of Lab 3 program--\n");
+    struct stat buf;
+	gid_t getgid(void);
+    pid_t pid = fork();
 
-            if (pid < 0)
-            {
-                perror("Fork failed");
-            }
-            if (pid == 0)
-            {
-                for (i = 1; i < argc; i++)
-                {
-			int chUID = getuid();
-			int chGID = getgid();
-			
-			if (chUID == cUID)
-			{
-				printf("Filename:%s",argv[i]);
-				printf("You have owner permissions:%s \n");
-			}
-			if (chGID == cGID)
-			{
-				printf("Filename:%s", argv[i]);
-				printf("You have general permissions: %s\n");
-			} else {
-				printf("Owner is ouside the user group\n");
-				}
-	
+    if (pid < 0) {
+      perror("Fork failed");
+    }
+    if (pid == 0) {
+      for (i = 1; i < argc; i++) {
+        stat(argv[i], &buf);
+        printf("\n File:%s => ", argv[i]);
 
-                }
-		exit(0);
-	    }
-            if (pid > 0)
-            {
-                wait(NULL);
-                system("ps -H");
-            }
-}
+        if (getuid() == buf.st_uid) {
+          printf("You have owner permissions: ");
+          if (buf.st_mode & 00400) {
 
+            printf(" read");
+
+          }
+          if (buf.st_mode & 00200) {
+
+            printf(" write");
+
+          }
+          if (buf.st_mode & 00100) {
+
+            printf(" execute");
+
+          }
+          printf("\n");
+
+        } else if (getgid() == buf.st_uid) {
+          printf("You have group permissions: ");
+          if (buf.st_mode & 00040) {
+
+            printf(" group_read");
+
+          }
+          if (buf.st_mode & 00020) {
+
+            printf(" group_write");
+
+          }
+          if (buf.st_mode & 00010) {
+
+            printf(" group_execute");
+
+          }
+
+        } else {
+          printf("You have group permissions: ");
+          if (buf.st_mode & 00004) {
+
+            printf(" general_read");
+
+          }
+          if (buf.st_mode & 00002) {
+
+            printf(" general_write");
+
+          }
+          if (buf.st_mode & 00001) {
+
+            printf(" general_execute");
+
+          }
+        }
+      }
+      exit(0);
+    }
+    if (pid > 0) {
+      wait(NULL);
+      system("\n\n ps -H");
+    }
+  }
